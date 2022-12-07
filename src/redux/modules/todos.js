7000beab624677a829b7ee0157bb68905207215c
정창원 <8500jcw@gmail.com>
2022-12-08 02:07:57 +0900

    초기세팅

---
+++ src/redux/modules/todos.js
@@ -0,0 +1,15 @@
+//action value
+
+//action creator
+
+//initialState
+const initialState = { todosList: [], detail: {} };
+//reducer
+const todos = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+//export
+export default todos;
---
+++ src/shared/Router.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Detail from "../pages/Detail";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route>
+          <Route path=":id" element={<Detail />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
